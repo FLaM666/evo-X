8c2014cac27843eebe8083725c46ba19306946b6
beleko <idgamerz@web.de>
2009-12-09 22:31:21 +0100

    320 Druid forms implented
    Signed-off-by: beleko <idgamerz@web.de>

--- src/game/Unit.h
+++ src/game/Unit.h
@@ -1567,6 +1567,7 @@ class MANGOS_DLL_SPEC Unit : public WorldObject
         void AddPetAura(PetAura const* petSpell);
         void RemovePetAura(PetAura const* petSpell);
 
+        uint32 GetModelForForm(ShapeshiftForm form);
     protected:
         explicit Unit ();
 
