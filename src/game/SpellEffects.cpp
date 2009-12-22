7f5fe04ff50f01f6fc898f2428cdce98b9be78fd
beleko <idgamerz@web.de>
2009-12-22 10:20:14 +0100

    [9045] Implement talent 48539 and ranks.
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>
    
    Also some typos in mangos.sql field order fixes (sql updates in past ahe
    been correct)
    
    Signed-off-by: beleko <idgamerz@web.de>

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -3018,6 +3018,7 @@ void Spell::EffectEnergize(uint32 i)
             level_multiplier = 4;
             break;
         case 31930:                                         // Judgements of the Wise
+        case 48542:                                         // Revitalize (mana restore case)
         case 63375:                                         // Improved Stormstrike
             damage = damage * unitTarget->GetCreateMana() / 100;
             break;
