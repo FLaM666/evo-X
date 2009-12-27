428bc1370d9bc87fed0be7a047fbae77d06264e2
beleko <idgamerz@web.de>
2009-12-28 00:10:56 +0100

    first part uf evo-X Emu soory Viste it had intersected and big big Thanks
    Signed-off-by: beleko <idgamerz@web.de>

--- src/evo-X_world/Main.cpp
+++ src/evo-X_world/Main.cpp
@@ -148,17 +148,16 @@ extern int main(int argc, char **argv)
     sLog.outString( "%s [world-daemon]", _FULLVERSION(REVISION_DATE,REVISION_TIME,REVISION_NR,REVISION_ID) );
     sLog.outString( "<Ctrl-C> to stop.\n\n" );
 
-    sLog.outTitle( "MM   MM         MM   MM  MMMMM   MMMM   MMMMM");
-    sLog.outTitle( "MM   MM         MM   MM MMM MMM MM  MM MMM MMM");
-    sLog.outTitle( "MMM MMM         MMM  MM MMM MMM MM  MM MMM");
-    sLog.outTitle( "MM M MM         MMMM MM MMM     MM  MM  MMM");
-    sLog.outTitle( "MM M MM  MMMMM  MM MMMM MMM     MM  MM   MMM");
-    sLog.outTitle( "MM M MM M   MMM MM  MMM MMMMMMM MM  MM    MMM");
-    sLog.outTitle( "MM   MM     MMM MM   MM MM  MMM MM  MM     MMM");
-    sLog.outTitle( "MM   MM MMMMMMM MM   MM MMM MMM MM  MM MMM MMM");
-    sLog.outTitle( "MM   MM MM  MMM MM   MM  MMMMMM  MMMM   MMMMM");
-    sLog.outTitle( "        MM  MMM http://getmangos.com");
-    sLog.outTitle( "        MMMMMM\n\n");
+    sLog.outTitle( "EEEEEEE EE                EE   EEEEEEEEEEEE           EE       EE    ");
+    sLog.outTitle( "EE       EE              EE    EE        EE            EE    EE    ");
+    sLog.outTitle( "MM        EE            EE     EE        EE             EE  EE     ");
+    sLog.outTitle( "MM         EE          EE      EE        EE               EE      ");
+    sLog.outTitle( "EEEEEEE     EE        EE       EE        EE   EEEEEEE    EEEE      ");
+    sLog.outTitle( "EE           EE      EE        EE        EE             EE  EE     ");
+    sLog.outTitle( "EE            EE    EE         EE        EE            EE    EE    ");
+    sLog.outTitle( "EE             EE  EE          EE        EE           EE      EE   ");
+    sLog.outTitle( "EEEEEEE          EE            EEEEEEEEEEEE          EE        EE  ");
+    sLog.outTitle( "        MM  MMM http://europe-wow.eu");
 
     sLog.outString("Using configuration file %s.", cfg_file);
 
