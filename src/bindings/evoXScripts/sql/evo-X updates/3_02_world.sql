9b1a1b9401837fb45dc9d555ca3e9ed6e7f5b2f5
Viste <lenchcic@evpanet.com>
2010-01-02 17:08:28 +0200

    * typo typo :(

--- src/bindings/evoXScripts/sql/evo-X updates/3_02_world.sql
+++
@@ -1,4 +0,0 @@
--- Violet hold
-UPDATE creature_template SET ScriptName="npc_door_seal_vh", unit_flags=33816580 WHERE entry=30896;
-UPDATE creature_template SET faction_A=35, faction_H=35 WHERE entry=30658;
-UPDATE creature_template SET faction_A=1720, faction_H=1720, ScriptName="mob_vh_dragons", minlevel=70,maxlevel=70,minhealth=7000,maxhealth=8000  WHERE entry IN (30660, 30661, 30662, 30663, 30664, 30666, 30667, 30668, 32191, 30695);
